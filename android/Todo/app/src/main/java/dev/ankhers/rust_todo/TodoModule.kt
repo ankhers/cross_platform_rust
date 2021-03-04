c86aa2ab8f716a1d109039a9c6bdc58ca821cd6a
Justin Wood <me@ankhers.dev>
2021-03-04 13:57:51 -0500

    Enable logging to the rust lib

--- android/Todo/app/src/main/java/dev/ankhers/rust_todo/TodoModule.kt
+++ android/Todo/app/src/main/java/dev/ankhers/rust_todo/TodoModule.kt
@@ -7,12 +7,15 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dev.ankhers.todo.Store
+import dev.ankhers.todo.setup
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TodoModule {
     @Provides
     fun provideStore(@ApplicationContext context: Context): Store {
+        // This setup function sets up logging
+        setup()
         return Store(context.getDatabasePath("todo.sqlite").absolutePath)
     }
 }
--- rust/todo/src/lib.rs
+++ rust/todo/src/lib.rs
@@ -1,9 +1,24 @@
 pub mod db;
 pub mod error;
 
+#[cfg(target_os = "android")]
+use android_logger::Config;
+#[cfg(target_os = "android")]
+use log::Level;
+
+#[cfg(not(target_os = "android"))]
+extern crate simple_logger;
+
 use crate::db::models::list::TodoList;
 use crate::db::models::list_item::TodoListItem;
 use crate::db::store::Store;
 use crate::error::Error;
+
+pub fn setup() {
+    #[cfg(target_os = "android")]
+    android_logger::init_once(Config::default().with_min_level(Level::Trace));
+    #[cfg(not(target_os = "android"))]
+    simple_logger::SimpleLogger::new().init().unwrap();
+}
 
 include!(concat!(env!("OUT_DIR"), "/todo.uniffi.rs"));
